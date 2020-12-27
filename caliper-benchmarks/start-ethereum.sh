e6e8f280ac980a0269221180ad61343f3b30b7ce
bharathreddypanyala <bhpa18@student.bth.se>
2020-12-27 15:13:05 +0100

    Add caliper benchmarks

---
+++ caliper-benchmarks/start-ethereum.sh
@@ -0,0 +1,1 @@
+cd ~/ethereum/caliper-benchmarks/networks/ethereum/1node; docker-compose up
---
+++ caliper-benchmarks/start-test.sh
@@ -0,0 +1,2 @@
+npx caliper launch master --caliper-benchconfig benchmarks/scenario/simple/config.yaml --caliper-workspace . --caliper-networkconfig networks/ethereum/1node/ethereum.json --caliper-bind-sut ethereum:latest
+
